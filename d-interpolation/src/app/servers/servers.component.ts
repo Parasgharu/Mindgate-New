9963076d93afe10d66d72393c4b4769404109226
Parasgharu <105363965+Parasgharu@users.noreply.github.com>
2022-06-03 18:54:39 +0530

    Add files via upload
    d

---
+++ d-interpolation/src/app/servers/servers.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+    allowNewServer :boolean=true;
+    serverCreationStatus :string="No Server was created";
+    serverName:string="";
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer=false;
+    }, 5000);
+  }
+
+  onCreateServer(){
+    this.serverCreationStatus="Server was Created !!"
+  }
+
+  // onUpdateServerName(event : Event){
+  //   this.serverName=(<HTMLInputElement> event.target).value;
+  // }
+  ngOnInit(): void {
+  }
+
+}
